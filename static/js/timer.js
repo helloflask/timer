1221e9597ba105939f9c61884d732c286fccd9db
Hui Li <withlihui@gmail.com>
2016-11-09 11:04:35 +0800

    progress bar issue fix

--- static/js/timer.js
+++ static/js/timer.js
@@ -24,39 +24,9 @@ function startTimer(duration, display) {
 
 // start timer
 jQuery(function ($) {
-    var Minutes = $("#minutes").html(),
-    display = $('#time');
+    var display = $('#time');
     startTimer(Minutes, display);
 });
-
-// progress bar
-var bar = new ProgressBar.Circle(container, {
-  color: '#eee',
-  // This has to be the same size as the maximum width to
-  // prevent clipping
-  strokeWidth: 4,
-  trailWidth: 2,
-  easing: 'linear',
-  duration: $("#duration").html(),
-  text: {
-    autoStyleContainer: false
-  },
-  from: { color: '#FFEA82', width: 2 },
-  to: { color: '#4CAF50', width: 4 },
-  // Set default step function for all animate calls
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
- var barText = $("#time").html();
-      circle.setText(barText);
-  }
-});
-bar.text.style.fontFamily = 'FranklinGothic, Verdana, Arial, sans-serif';
-bar.text.style.fontSize = '80px';
-bar.text.style.fontWeight = 'bold';
-bar.text.style.color = '#000';
-
-bar.animate(1.0);  // Number from 0.0 to 1.0
 
 // center the content
 $(window).resize(function(){
